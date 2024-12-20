5f5d2f14120b917029268efbc8ad8fd395c46536
guglielmosanna <guglielmosanna05@gmail.com>
2024-12-20 16:09:19 +0000

    modified hello

--- hello/hello.c
+++ hello/hello.c
@@ -4,5 +4,13 @@ int main()
 {
     printf("Hello World!!!\n\n\n");
 
+    char name[32];
+
+    printf("inserisci un nome: ");
+
+    scanf("%s", name);
+
+    printf("\nhello\t%s\n\n\n", name);
+    
     return 0;
 }
